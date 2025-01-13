ab38782e3573d3854815ac9aa21ed8b14ec33562
Roberto <robercantalejo@gmail.com>
2025-01-13 19:54:18 +0000

    Creo las variables y sus arrays

---
+++ app.js
@@ -0,0 +1,7 @@
+/* Creo las variables que voy a usar para almacenar los distintos strings */
+
+let pronoun = ['the', 'our', 'somebody', 'that'];
+let adj = ['fantastic', 'mighty', 'amazing', 'agents of', 'brotherhood of', 'league of' ];
+let noun = ['avengers', 'four', 'spider-men', 'mutants', 'Chronos', 'heroes', 'villains' ];
+let domains = ['.com', '.org', '.net', '.edu', '.cn']
+
